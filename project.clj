b27d629fff3006132972ba0ebef37ec210fc1691
Theo Pack <tf.pack@gmail.com>
2014-06-30 14:12:24 +0200

    Init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-emoji "0.1.0-SNAPSHOT"
+  :description "Little fun project with clojure + emoji"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
