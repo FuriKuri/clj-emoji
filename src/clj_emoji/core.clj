feede0740d147bba17f63500ca545b84c33f51f2
Theo Pack <tf.pack@gmail.com>
2014-07-18 11:09:21 +0200

    Add function to ask for fn or macro

--- src/clj_emoji/core.clj
+++ src/clj_emoji/core.clj
@@ -4,3 +4,11 @@
   "Create a new function for an existing function."
   [emoji-fn alias-fn]
   `(defn ~emoji-fn [& args#] (apply ~alias-fn args#)))
+
+(defmacro fn-or-macro?
+  "Return true if x is a function or a macro."
+  [x]
+  (let [result (resolve (symbol x))]
+    (if (nil? result)
+      false
+      true)))
