ea8c2d3817a851a55a50ea8a581d52175437f647
Theo Pack <tf.pack@gmail.com>
2014-07-25 15:01:56 +0200

    Save value mapping

--- src/clj_emoji/core.clj
+++ src/clj_emoji/core.clj
@@ -1,16 +1,32 @@
 (ns clj-emoji.core)
 
+(def mapping (atom {}))
+
+(defn add-value [x y]
+  (swap! mapping assoc x y))
+
+(defn get-value [x]
+  (if (contains? @mapping x)
+    (@mapping x)
+    x))
+
+(defn map-it [x]
+  (map get-value x))
+
 (defmacro emoji
   "Create a new function for an existing function."
   [emoji-fn alias-fn]
-  `(defn ~emoji-fn [& args#] (apply ~alias-fn args#)))
-
-(defmacro fn-or-macro?
-  "Return true if x is a function or a macro."
-  [x]
   (try
-    (let [result (resolve (symbol x))]
-      (if (nil? result)
-        false
-        true))
-    (catch ClassCastException e false)))
+    (if (not (nil? (resolve alias-fn)))
+      (let [emoji-sym (symbol emoji-fn)]
+        `(defn ~emoji-sym [& args#] (apply ~alias-fn (map-it args#))))
+      (add-value alias-fn emoji-fn))
+    (catch ClassCastException e (add-value alias-fn emoji-fn))))
+
+(defmacro init
+  "Maps some functions."
+  []
+  `(do
+    (emoji "🐙" println)
+    (emoji "🎅" +)
+    (emoji "⌚" -)))
--- test/clj_emoji/core_test.clj
+++ test/clj_emoji/core_test.clj
@@ -12,16 +12,10 @@
       (emoji my-str str)
       (is (=
            (str "a" "bc" "def")
-           (my-str "a" "bc" "def"))))))
-
-(deftest fn-or-macro-fn
-  (testing "println is a function"
-    (is (fn-or-macro? println)))
-  (testing "if-not is a macro"
-    (is (fn-or-macro? if-not)))
-  (testing "my-fn is not a funtion or a macro"
-    (is (not (fn-or-macro? my-fn))))
-  (testing "a string is not a funtion or a macro"
-    (is (not (fn-or-macro? "hello"))))
-  (testing "a number is not a funtion or a macro"
-    (is (not (fn-or-macro? 42)))))
+           (my-str "a" "bc" "def")))))
+  (testing "Map str-fn to own function as string"
+    (do
+      (emoji "my-str-fn" str)
+      (is (=
+           (str "a" "bc" "def")
+           (my-str-fn "a" "bc" "def"))))))
